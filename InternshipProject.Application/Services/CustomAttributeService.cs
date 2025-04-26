46747d421a88a74067751756a933f4fb5e3b6fb6
BaverYldz <omerbaveryildiz545@gmail.com>
2025-04-26 23:00:01 +0300

    project refrence errors fixed

--- InternshipProject.Application/Services/CustomAttributeService.cs
+++ InternshipProject.Application/Services/CustomAttributeService.cs
@@ -1,15 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using InternshipProject.Infrastructure.Data;
+using System;
+using System.Reflection.Metadata;
 
 
 namespace InternshipProject.Application.Services
 {
-    public class CustomAttributeService
+    public class CustomAttributeService : ICustomAttributeService
     {
-        
-        
+        private readonly AppDbContext _context;
+
+        public CustomAttributeService(AppDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public Task<CustomAttribute> CreateAsync(CustomAttribute customAttribute)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<CustomAttribute>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CustomAttribute> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(Guid id, CustomAttribute customAttribute)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
